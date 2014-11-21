1fa99ed0a883be3e48f3e53a0ffdd3cdf220b37b
jepetko <golbang.k@gmail.com>
2014-11-21 13:49:40 +0100

    remove unused code

--- WebContent/js/app/application.js
+++ WebContent/js/app/application.js
@@ -1,36 +1,5 @@
 angular.element(window).ready( function() {
     var parent = $('#main');
     angular.module('pizzeria-app', ['gettext', 'users', 'shared', 'shopping-app', 'orders-app']);
-    var appInjector = angular.bootstrap(parent,['pizzeria-app']);
-    
-    /*
-    console.log(appInjector);
-    var log = appInjector.get('$log');
-    console.log( log.error );
-    
-    
-    function formatError(arg) {
-        if (arg instanceof Error) {
-          if (arg.stack) {
-            arg = (arg.message && arg.stack.indexOf(arg.message) === -1)
-                ? 'Error: ' + arg.message + '\n' + arg.stack
-                : arg.stack;
-          } else if (arg.sourceURL) {
-            arg = arg.message + '\n' + arg.sourceURL + ':' + arg.line;
-          }
-        }
-        return arg;
-      }    
-    
-    log.error = function () {
-        var args = [];
-        angular.forEach(arguments, function(arg) {
-          //args.push(formatError(arg));
-        	args.push(arg);
-        });
-        console.log(args);
-        //return logFn.apply(console, args);
-        return {1: 'helo'};
-    }; 
-    */
+    angular.bootstrap(parent,['pizzeria-app']);
 });
