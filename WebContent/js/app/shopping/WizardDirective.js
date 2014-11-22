498da1aba28a3d270d5de64b41205d7e36da4791
jepetko <golbang.k@gmail.com>
2014-11-22 18:43:23 +0100

    active wizard element + unit test

--- WebContent/js/app/shopping/WizardDirective.js
+++ WebContent/js/app/shopping/WizardDirective.js
@@ -2,9 +2,10 @@
 	"use strict";
 	
 	angular.module('wizard-components',[])
-	.directive('wizard', function() {
+	.directive('wizard', ['$rootScope', '$location', function($rootScope, $location) {
 		
-		var link = function(scope, el, attrs) {			
+		var link = function(scope, el, attrs) {
+
 		};
 		
 		return {
@@ -14,34 +15,50 @@
 			controller: function($scope) {
 				$scope.pages = [ {
 					label : 'Choose product',
-					link : '#!/select'
+					link : '#!/select',
+					css : ''
 				}, {
 					label : 'Your shopping bag',
-					link : '#!/bag'
+					link : '#!/bag',
+					css : ''
 				}, {
 					label : 'Your address',
-					link : '#!/address'
+					link : '#!/address',
+					css : ''
 				}, {
 					label : 'Finish',
-					link : '#!/finish'
+					link : '#!/finish',
+					css : ''
 				}, {
 					label : 'Orders',
-					link : '#!/orders'
+					link : '#!/orders',
+					css : ''
 				}];
 				
 				$scope.isOrdersTab = function(label) {
 					return label == 'Orders';
 				};
+				
+				$rootScope.$on('$locationChangeSuccess', function(newState, oldState) {					
+					var path = $location.path();
+					angular.forEach($scope.pages, function(el) {
+						if(path === el.link.replace(/^#!/,'')) {
+							el.css = 'active';
+						} else {
+							el.css = '';
+						}
+					});
+				});
 			},
 			restrict: 'E',	
 			replace: true,
 			transclude: true,				
 			template: '<div class="panel panel-default"><div class="panel-body">' +
 						'<ul class="nav nav-pills" role="tablist">' +
-							'<li role="presentation" ng-repeat="p in pages"><a href="{{p.link}}">{{p.label | translate}}<span ng-if="isOrdersTab(p.label)"> ({{c()}})</span></a></li>' +
+							'<li role="presentation" ng-repeat="p in pages" ng-class="p.css"><a href="{{p.link}}">{{p.label | translate}}<span ng-if="isOrdersTab(p.label)"> ({{c()}})</span></a></li>' +
 						'<ul>' +
 					'</div></div>'
 		};
-	});	
+	}]);	
 	
 })();
--- WebContent/tests/WizardDirective.test.js
+++ WebContent/tests/WizardDirective.test.js
@@ -1,6 +1,6 @@
 describe('wizard-components', function() {
 	
-	var scope, compile;
+	var rootScope, scope, compile, location;
 	
 	var compileTpl = function(tpl) {
 		var link = compile(tpl);
@@ -11,19 +11,24 @@ describe('wizard-components', function() {
 
 	beforeEach(module('gettext'));
 	beforeEach(module('wizard-components'));
-	beforeEach(inject(function($rootScope, $compile) {
+	beforeEach(inject(function($rootScope, $compile, $location) {
+		rootScope = $rootScope;
 		scope = $rootScope.$new();
 		scope.getOrderCount = function() {
 			return 3;
 		};
 		compile = $compile;
+		location = $location;
 	}));
 	
 	describe('Wizard', function() {
-		it('should contain 4 items', function() {
-			
-			var element = compileTpl('<wizard c="getOrderCount()"></wizard');
-						
+		
+		var element = null;
+		beforeEach( function() {
+			element = compileTpl('<wizard c="getOrderCount()"></wizard');
+		});
+		
+		it('should contain 5 items', function() {						
 			var lis = $(element).find('li');
 			expect(lis.length).toBe(5);
 			
@@ -33,6 +38,17 @@ describe('wizard-components', function() {
 				expect($(el).text()).toEqual(labels[i]);
 				i++;
 			});
+		});
+		
+		it('should display the 2nd item as selected when the current URL is #!/bag', function() {
+			location.path('/bag');
+			rootScope.$digest();
+			
+			var lis = $(element).find('li');
+			var second = $(lis[1]);
+						
+			var classes = second.attr('class').split(' ');
+			expect( $.inArray('active', classes) ).toBeGreaterThan(-1);			
 		});
 	});
 	
