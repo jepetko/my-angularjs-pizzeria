30df2f0ba18d7793bd8b5994cbcf42e2109e772b
jepetko <golbang.k@gmail.com>
2014-11-23 13:18:54 +0100

    default language is en; re-minified...

--- WebContent/js/app/application.js
+++ WebContent/js/app/application.js
@@ -2,7 +2,7 @@ angular.element(window).ready( function() {
     var parent = $('#main');
     angular.module('pizzeria-app', ['gettext', 'shared', 'shopping-app', 'orders-app'])    
     .run(['gettextCatalog', function(gettextCatalog) {
-        gettextCatalog.setCurrentLanguage('de');
+        gettextCatalog.setCurrentLanguage('en');
         gettextCatalog.debug = true;
 	}] );
     angular.bootstrap(parent,['pizzeria-app']);
--- WebContent/js/app/login.js
+++ WebContent/js/app/login.js
@@ -2,7 +2,7 @@ angular.element(window).ready( function() {
 	var parent = $('#login');
 	angular.module('login', ['gettext'])
 	.run(['gettextCatalog', function(gettextCatalog) {
-		gettextCatalog.setCurrentLanguage('de');
+		gettextCatalog.setCurrentLanguage('en');
         gettextCatalog.debug = true;		
 	}] );
     angular.bootstrap(parent,['login']);
