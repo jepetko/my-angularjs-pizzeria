8f559b8370e4e1ffe56c0b680fd17e1ecb9959b7
katarina <golbang.k@gmail.com>
2015-05-10 21:18:39 +0200

    protractor configuration

---
+++ WebContent/specs/AddProductsToBag.js
@@ -0,0 +1,7 @@
+describe('Adding products to the bag', function() {
+
+	it('adds 3 margarithas to the bag if the user puts the value 3 into the according text field', function() {
+		expect(true).toBe(true);
+	});
+	
+});
--- runner/Gruntfile.js
+++ runner/Gruntfile.js
@@ -1,20 +1,40 @@
 module.exports = function(grunt) {
 
-    var continuousIntegrationMode = grunt.option('ci') || false;
+	var continuousIntegrationMode = grunt.option('ci') || false;
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+	grunt.initConfig({
+		pkg : grunt.file.readJSON('package.json'),
 
-        karma: {
-            unit: {
-                configFile: 'karma.conf.js'/*,
-                singleRun: continuousIntegrationMode,
-                reporters: continuousIntegrationMode ? ['junit'] : ['progress', 'html', 'story']*/
-            }
-        }
-    });
+		karma : {
+			unit : {
+				configFile : 'karma.conf.js'/*,
+				                singleRun: continuousIntegrationMode,
+				                reporters: continuousIntegrationMode ? ['junit'] : ['progress', 'html', 'story']*/
+			}
+		},
 
-    grunt.loadNpmTasks('grunt-karma');
+		protractor : {
+			options : {
+				confgFile : "node_modules/protractor/example/conf.js", // Default config file 
+				keepAlive : true, // If false, the grunt process stops when the test fails. 
+				noColor : false, // If true, protractor will not use colors in its output. 
+				args : {
+					// Arguments passed to the command 
+				}
+			},
+			e2e : { // Grunt requires at least one target to run so you can simply put 'all: {}' here too. 
+				options : {
+					configFile : "e2e.conf.js", // Target-specific config file 
+					args : {}
+					// Target-specific arguments 
+				}
+			}
 
-    grunt.registerTask('default', ['karma']);
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-karma');
+	grunt.loadNpmTasks('grunt-protractor-runner');
+
+	grunt.registerTask('default', [ 'karma' ]);
 };
